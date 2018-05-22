aeca0263a8c5c30133cb17a9c60141be0c8a6ec7
cmoniere <camillemvdd@gmail.com>
2018-05-22 20:05:35 +0200

    oui

--- src/Filter/convolutionfilter.hpp
+++ src/Filter/convolutionfilter.hpp
@@ -10,12 +10,12 @@ public:
     ConvolutionFilter( QString _name, std::vector<int> _matrix);
     ~ConvolutionFilter();
     virtual int convolutionMatrix( std::vector<int> A);
-    void process(FastImage* _buffIn, FastImage* _buffOut);
+    virtual void process(FastImage* _buffIn, FastImage* _buffOut);
 private:
     std::vector<int> matrix; // /!\ matrix est de dimension (9,1) m(1,1) = matrix(1), m(3,3) = matrix(9). De plus, matrix est la version retourné de la vrai matrice.
 protected:
     void set_mat( std::vector<int> _matrix);
     std::vector<int> get_mat();
-
+    virtual int convolutionMatrix( std::vector<int> A);s
 };
 #endif // CONVOLUTIONFILTER_HPP
